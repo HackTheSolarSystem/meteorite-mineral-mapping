32ecd4c739cf71fc8c8c3e4ca7b0a7f70e12b2fc
Clyde H Shaffer III <clydeshaffer@gmail.com>
2019-02-09 23:02:56 -0500

    calculate average intensities per element per mineral

--- src/calibrator/main.py
+++ src/calibrator/main.py
@@ -4,30 +4,6 @@ import numpy as np
 from PIL import Image
 import json
 
-with open("standards.json", 'r') as f:
-	standards_dict = json.load(f)
-
-elem_names = []
-elem_images = []
-elem_dimages = []
-
-width = 0
-height = 0
-
-elements = standards_dict["elements"]
-
-for element in elements:
-	img = Image.open(elements[element])
-	elem_names.append(element)
-	elem_images.append(img)
-	elem_dimages.append(cuda.to_device(img))
-	width = img.width
-	height = img.height
-
-
-maskBuf = np.zeros((height, width), dtype = np.int32)
-device_maskBuf = cuda.to_device(maskBuf)
-
 @cuda.jit
 def map_nonzero(inImage, outImage):
 	for x in range(0, width):
@@ -37,16 +13,51 @@ def map_nonzero(inImage, outImage):
 			else:
 				outImage[y,x] = 0
 
+@cuda.jit
+def map_mask(inImage, maskImage, outImage):
+	for x in range(0, width):
+		for y in range(0, height):
+			if maskImage[y,x] != 0:
+				outImage[y,x] = inImage[y,x]
+			else:
+				outImage[y,x] = 0
+
 @cuda.reduce
 def sum_reduce(a, b):
 	return a+b
 
-testmin = standards_dict["minerals"]["iron"]
-testMinMaskImage = Image.open(testmin["maskFile"])
+with open("standards.json", 'r') as f:
+	standards_dict = json.load(f)
 
-dTestMin = cuda.to_device(testMinMaskImage)
 
-map_nonzero(dTestMin, device_maskBuf)
-maskpixels = sum_reduce(np.ravel(device_maskBuf))
+width = 0
+height = 0
 
-print(maskpixels)
+elements = standards_dict["elements"]
+minerals = standards_dict["minerals"]
+
+element_dimages = {}
+
+for element in elements:
+	img = Image.open(elements[element])
+	element_dimages[element] = cuda.to_device(img)
+	width = img.width
+	height = img.height
+
+maskBuf = np.zeros((height, width), dtype = np.int32)
+device_maskBuf = cuda.to_device(maskBuf)
+
+for mineral in minerals:
+	maskImg = Image.open(minerals[mineral]["maskFile"]);
+	maskDImage = cuda.to_device(maskImg)
+
+	map_nonzero(maskDImage, device_maskBuf)
+	maskpixels = sum_reduce(np.ravel(device_maskBuf))
+	minerals[mineral]["intensity"] = {}
+	print(mineral)
+	for element in elements:
+		map_mask(element_dimages[element], maskDImage, device_maskBuf)
+		elemTotalIntensity = sum_reduce(np.ravel(device_maskBuf))
+		elemAverageIntensity = elemTotalIntensity / maskpixels
+		minerals[mineral]["intensity"][element] = elemAverageIntensity
+		print("\t" + element + ": " + str(elemAverageIntensity))
